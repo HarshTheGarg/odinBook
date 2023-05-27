04dc48217fff98f995e82f308e4d35ee19e664b0
Harsh Garg <gargharsh2512@gmail.com>
2023-05-27 19:17:20 +0530

    Add index route and eslint for the server

--- server/router.js
+++ server/router.js
@@ -1,12 +1,26 @@
-const app = require("express")();
-require('dotenv').config();
+const express = require("express");
+const app = express();
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+const index = require("./routes/index");
+app.use("/", index);
+
+app.get("/*", (req, res) => {
+  res.status(404);
+  res.json({
+    msg: "Page not found",
+  });
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
