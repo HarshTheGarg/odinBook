bb2a8971f18400287197ff60441ea68b34098b6e
Harsh Garg <gargharsh2512@gmail.com>
2023-06-03 20:18:47 +0530

    Remove redux logger

--- client/src/redux/app/store.js
+++ client/src/redux/app/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import cuReducer from "../features/currentUser/cuSlice";
 
 const store = configureStore({
   reducer: {
     cu: cuReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
