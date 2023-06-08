8991549877d0f6c58e9d11d6fbbaf8f3466437b7
Harsh Garg <gargharsh2512@gmail.com>
2023-06-08 20:49:21 +0530

    Display appropriate button in the find friends list

--- client/src/components/Friends/Find/ListUser/ListUser.js
+++ client/src/components/Friends/Find/ListUser/ListUser.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 function ListUser({ type, user }) {
+  const [ requested, setRequested ] = useState(type == "requested" ? true : false);
+
+  const request = () => {
+    
+    if(requested) {
+      // Un-request
+      console.log("Un-Request");
+    } else {
+      // Request
+      console.log("Request");
+    }
+
+    setRequested(!requested);
+
+  };
+
   return (
     <>
       {user.username}
-      {type=="requested" ? "requested" : "Add"}
+      <button onClick={request} className={requested ? "requested" : null}>
+        {requested ? "Requested" : "Request"}
+      </button>
     </>
   );
 }
 
 ListUser.propTypes = {
   type: PropTypes.string,
-  user: PropTypes.object
+  user: PropTypes.object,
 };
 
 export default ListUser;
