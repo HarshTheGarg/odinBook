6900fe67b309dd98e9729d819bc8ce0248027703
Harsh Garg <gargharsh2512@gmail.com>
2023-06-14 21:53:56 +0530

    Remove the extra routes from index

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,11 @@
 const express = require("express");
 const router = express.Router();
 
-// Passport to authenticate the user
-const passport = require("passport");
-
 // TODO Enable while deploying
 /* const path = require("path")
 router.use(express.static(path.join(__dirname, "../../client/dist/")));
  */
+
 // Route to authenticate the users
 router.use("/auth", require("./auth/auth"));
 
@@ -17,27 +15,7 @@ router.get("/test", (req, res) => {
   res.json({ msg: "I am a test" });
 });
 
-
-// TODO remove while deploying
-router.get(
-  "/protected",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.json({ success: true, user: req.user });
-  }
-);
-
 router.use("/user", require("./user"))
-
-/* 
-router.get(
-  "/userData",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.json({ success: true, user: req.user });
-  }
-);
- */
 
 // TODO Enable while deploying
 /* router.use("*", (req, res) => {
