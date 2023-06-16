0a0bd139a464ce0224ebfbff6e8558f635195c2d
Harsh Garg <gargharsh2512@gmail.com>
2023-06-16 17:59:44 +0530

    Like posts

--- client/src/components/Post/PostCard/PostCard.jsx
+++ client/src/components/Post/PostCard/PostCard.jsx
@@ -1,14 +1,58 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
-function PostCard({post}) {
+function PostCard({ post }) {
+  const navigate = useNavigate();
+
+  const [likes, setLikes] = useState(0);
+
+  const likePost = () => {
+    setLikes((prevState) => prevState + 1);
+    fetch("http://localhost:3000/post/like", {
+      method: "POST",
+      headers: {
+        Authorization: localStorage.getItem("token"),
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ postId: post._id }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        if (!result.success) {
+          return result;
+        }
+      })
+      .catch((err) => {
+        if (err.status == 401) {
+          navigate("/Unauthorized");
+        }
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    console.log(post);
+    setLikes(post.likes.length);
+  }, []);
+
   return (
-    <div>{post.caption}</div>
+    <>
+      <div>
+        {post.caption} - {post.author.username}
+      </div>
+      <div>
+        <button onClick={likePost}>Like</button> {likes}
+      </div>
+      <div>Comments:</div>
+    </>
   );
 }
 
 PostCard.propTypes = {
-  post: PropTypes.object
+  post: PropTypes.object,
 };
 
 export default PostCard;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -8,16 +8,16 @@ router.get("/", (req, res) => {
 });
 
 router.get("/all", async (req, res) => {
-  await Post.find({"author": {$in: [...req.user.friends, req.user._id]}})
-    .sort({"dateTime": -1})
+  await Post.find({ author: { $in: [...req.user.friends, req.user._id] } })
+    .sort({ dateTime: -1 })
     .populate("author", "email username")
+    .populate("likes", "username")
     .then((result) => {
       res.json({ success: true, posts: result });
     });
 });
 
 router.post("/create", (req, res, next) => {
-  console.log(req.body.caption);
   const post = new Post({
     caption: req.body.caption,
     author: req.user._id,
@@ -28,6 +28,20 @@ router.post("/create", (req, res, next) => {
     next(err);
   });
   res.json({ success: true, post });
+});
+
+router.post("/like", async (req, res, next) => {
+  const post = await Post.findById(req.body.postId);
+  post.likes.push(req.user._id);
+
+  post
+    .save()
+    .then(() => {
+      res.json({ success: true });
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
