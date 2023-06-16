2d1d4c658cf14f8ec2817dd863c199df9b7fe8ec
Harsh Garg <gargharsh2512@gmail.com>
2023-06-16 17:12:43 +0530

    Load when creating a post

--- client/src/components/Post/Create/CreatePost.jsx
+++ client/src/components/Post/Create/CreatePost.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { endLoading, startLoading } from "../../../redux/features/loader/loaderSlice";
 
 function CreatePost() {
-
   const [caption, setCaption] = useState("");
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const changeCaption = (e) => {
     setCaption(e.target.value);
@@ -12,37 +14,43 @@ function CreatePost() {
 
   const submitPost = (e) => {
     e.preventDefault();
-    console.log(caption);
+    dispatch(startLoading());
+
     fetch("http://localhost:3000/post/create", {
       method: "POST",
       headers: {
         Authorization: localStorage.getItem("token"),
-        "content-type": "application/json"
+        "content-type": "application/json",
       },
-      body: JSON.stringify({caption}),
-    }).then((response) => {
-      return response.json();
-    }).then((result) => {
-      if (result.success) {
-        navigate("/");
-      } else {
-        throw result;
-      }
+      body: JSON.stringify({ caption }),
     })
-    .catch((err) => {
-      if(err.status == 401) {
-        navigate("/Unauthorized");
-      }
-      console.log(err);
-    });
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        if (result.success) {
+          navigate("/");
+        } else {
+          throw result;
+        }
+      })
+      .catch((err) => {
+        if (err.status == 401) {
+          dispatch(endLoading());
+          navigate("/Unauthorized");
+        }
+        console.log(err);
+      });
   };
 
   return (
     <>
       <div>CreatePost</div>
       <form>
-        <input placeholder="Caption" onChange={changeCaption}/>
-        <button type="submit" onClick={submitPost}>Submit</button>
+        <input placeholder="Caption" onChange={changeCaption} />
+        <button type="submit" onClick={submitPost}>
+          Submit
+        </button>
       </form>
     </>
   );
